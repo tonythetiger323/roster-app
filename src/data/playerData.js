4944f64e28aad115bf8437745d926233cca5f2a0
Tony Greeley <tonygreeley@Tonys-iMac.local>
2021-02-06 11:05:45 -0600

    Updated player data.

--- src/data/playerData.js
+++ src/data/playerData.js
@@ -1,14 +1,14 @@
 //playerData.js
-const alexHobbies = ["dancing", "making YouTube videos", "fishing"];
-const joseHobbies = ["travel", "eating burgers", "skiiing"];
-const justinHobbies = ["hunting wabbits", "Sunshine Kids volunteer", "grilling"];
-const yordanHobbies = []
-const luisHobbies = []
+const rizzoHobbies = ["dancing", "making YouTube videos", "fishing"];
+const báezHobbies = ["travel", "eating burgers", "skiiing"];
+const contrerasHobbies = ["hunting wabbits", "Sunshine Kids volunteer", "grilling"];
+const kbHobbies = []
+const happHobbies = []
 
 export const playerData = [
-  { id: 1, firstName: "Alex", lastName: "Bregman", hobbies: alexHobbies, jerseyNumber: 2 },
-  { id: 4, firstName: "Yordan", lastName: "Alvarez", hobbies: yordanHobbies, jerseyNumber: 44 },
-  { id: 3, firstName: "Jose", lastName: "Altuve", hobbies: joseHobbies, jerseyNumber: 27 },
-  { id: 5, firstName: "Justin", lastName: "Verlander", hobbies: justinHobbies, jerseyNumber: 35 },
-  { id: 2, firstName: "Luis", lastName: "Garcia", hobbies: luisHobbies, jerseyNumber: 77 },
+  { id: 1, firstName: "Anthony", lastName: "Rizzo", hobbies: rizzoHobbies, jerseyNumber: 44 },
+  { id: 4, firstName: "Javier", lastName: "Báez", hobbies: báezHobbies, jerseyNumber: 9 },
+  { id: 3, firstName: "Wilson", lastName: "Contreras", hobbies: contrerasHobbies, jerseyNumber: 40 },
+  { id: 5, firstName: "Kris", lastName: "Bryant", hobbies: kbHobbies, jerseyNumber: 17 },
+  { id: 2, firstName: "Ian", lastName: "Happ", hobbies: happHobbies, jerseyNumber: 8 },
 ]
