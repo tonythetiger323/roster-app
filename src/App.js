8e73f56fe1a00d811a5ec72ddea03bf0e1b7f763
Tony Greeley <tonygreeley@Tonys-iMac.local>
2021-02-06 11:27:59 -0600

    Added player component to App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Welcome from "./components/Welcome";
+import Player from "./components/Player";
 
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <Player />
     </div>
   );
 }
